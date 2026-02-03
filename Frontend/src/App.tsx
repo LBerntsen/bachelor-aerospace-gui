9d477a6c37c2c9e5047a6faa39f492244d33237e
Markusgard <markus.solgard@gmail.com>
2026-02-03 16:09:42 +0100

    tester dummy data

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Testing from "./state/telemetry/testing.tsx";
 import WebSocketProvider from "./state/websocket/WebSocketProvider.tsx";
-
+import TestChart from "./components/TestChart.tsx";
 export default function App() {
     return (
         <>
+            <TestChart/>
             <WebSocketProvider/>
-            <Testing/>
+            
         </>
     );
 }
---
+++ Frontend/src/components/TestChart.tsx
@@ -0,0 +1,50 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { useSelector } from "react-redux";
+import { selectTelemetryValuesById } from "../state/telemetry/telemetrySlice";
+
+const TestChart = () => {
+
+const rawData = useSelector(selectTelemetryValuesById("sensor2")) || [];
+
+const data = rawData
+  .slice(-100) // keep last 100
+  .map((value, index) => ({
+    timestamp: Date.now() + index,
+    value
+  }));
+
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+
+        <XAxis
+          dataKey="timestamp"
+          type="number"
+          domain={['auto', 'auto']}
+          tickFormatter={(time) =>
+            new Date(time).toLocaleTimeString()
+          }
+        />
+
+        <YAxis />
+
+        <Tooltip
+          labelFormatter={(time) =>
+            new Date(time).toLocaleTimeString()
+          }
+        />
+
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke="#8884d8"
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default TestChart;
