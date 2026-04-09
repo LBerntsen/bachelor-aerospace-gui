fc5764e86cacb572618da48951a12c9930aa72af
Lukas Berntsen <lukasberntsen1@gmail.com>
2026-04-09 14:32:08 +0200

    La til app mode funksjon

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -6,13 +6,12 @@ import {Provider} from "react-redux";
 import {store} from "./state/store.ts";
 import SignalRProvider from "./state/signalr/SignalRProvider.tsx";
 import SponsorDataProvider from "./state/sponsor/SponsorDataProvider.tsx";
-
-const appMode = import.meta.env.VITE_APP_MODE;
+import {isOperator} from "./state/utility.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider store={store}>
-          {appMode === "operator" ? <SignalRProvider/> : <SponsorDataProvider/>}
+          {isOperator() ? <SignalRProvider/> : <SponsorDataProvider/>}
           <App />
       </Provider>
   </StrictMode>,
--- Frontend/src/state/utility.ts
+++ Frontend/src/state/utility.ts
@@ -1,1 +1,12 @@
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+export function isOperator()
+{
+    const appMode = import.meta.env.VITE_APP_MODE;
+    return appMode === "operator";
+}
+
+export function isSponsor()
+{
+    return !isOperator();
+}
