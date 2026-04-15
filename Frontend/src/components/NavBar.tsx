16a4b2e16fcd4067c4a0d90b57bdd33aa4ef87cc
Markusgard <markus.solgard@gmail.com>
2026-04-15 12:54:14 +0200

    changes to NavBar + ConsoleSend command

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,29 +1,30 @@
+import {Link} from "react-router-dom"
 import phoenixLogo from "../assets/Pheonix_logo.jpg";
 import {selectState} from "../state/telemetry/telemetrySlice.ts";
 import {useSelector} from "react-redux";
-import { NavLink } from "react-router-dom";
 
 export default function Navbar(){
     return (
         <div className=" ">
-            <div className="flex flex-col bg-black sm:gap-6 sm:py-4">
+            <div className="flex flex-col bg-black p-8 sm:gap-6 sm:py-4">
             <div className="flex flex-row items-center font-bold">
                 <img className="rounded-lg" width={64}src={phoenixLogo}/>
                 <div>
                     <div className="flex justify-center gap-4 items-center">
                         <h1 className="text-white text-xl">Phoenix 2</h1>
-                        <BackendState/>
+                        <div className="min-w-20 text-center">
+                            <BackendState />
+                        </div>
                     </div>
                     <h1 className="text-white text-xs ">Ground Control System</h1>
                 </div>
             </div>
                 <div className="sm:gap-6 flex flex-col">
-                    <NavLink to="/" end className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Dashboard</NavLink>
-                    <NavLink to="/propulsion" className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Propulsion</NavLink>
-                    <NavLink to="/avionics" className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Avionics</NavLink>
-                    <NavLink to="/recovery" className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Recovery</NavLink>
-                    <NavLink to="/mission-archive" className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Mission Archive</NavLink>
-                    <NavLink to="/settings" className={({ isActive }) => `text-white p-2 rounded tracking-[0.25em] transition-colors ${isActive ? "bg-[#1e293b]" : "hover:bg-[#1e293b]"}`}>Settings</NavLink>
+                    <Link to ="/" className="text-white hover:bg-[#1e293b] p-2 rounded tracking-[0.25em]">Dashboard </Link>
+                    <Link to ="/propulsion" className="text-white hover:bg-[#1e293b] p-2 rounded tracking-[0.25em]">Propulsion </Link>
+                    <Link to ="/" className="text-white hover:bg-[#1e293b] p-2 rounded tracking-[0.25em]">Avionics</Link>
+                    <Link to ="/recovery" className="text-white hover:bg-[#1e293b] p-2 rounded tracking-[0.25em]">Recovery</Link>
+                    <Link to ="/" className="text-white hover:bg-[#1e293b] p-2 rounded tracking-[0.25em]">Mission Archive</Link>
                 </div>
             </div>
         </div>
